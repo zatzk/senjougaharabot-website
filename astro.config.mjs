bfe0dad98e83b56e335053880431e2018bc2c429
Jean Luiz <zatzk@outlook.com>
2022-09-06 09:35:42 -0300

    config github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zatzk.github.io',
 	integrations: [tailwind()],
   vite: {
     ssr: {
